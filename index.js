a709d13cd628d1993165647fe0886fbbd901c5a7
sadeep kawshalya <kawshalyasadeep@gmail.com>
2022-09-03 12:25:15 +0530

    Bug Fixed

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ async function add_new_data() {
     sesc = data2.entries
     console.log(sesc);
 
-    for (let b = 2; b < 3; b++) {
+    for (let b = 3; b < 4; b++) {
 
         let se = sesc[b].set_in_context.season
         const response = await fetch(
